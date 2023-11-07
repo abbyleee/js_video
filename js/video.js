c88f8a972f9327621a631075213e23a7a436c041
abbyleee <113362061+abbyleee@users.noreply.github.com>
2023-11-07 14:41:59 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,19 @@
-var video;
+var video = document.querySelector(".video");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	autoplay = false;
 });
+
 
 document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
+	video.play();
+
 });
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+   video.pause();
+
+});
